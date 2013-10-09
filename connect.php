38647fd68cc954aff7ddc66ca57a546883279ab3
Sergio Majluf <sergio.majluf@gmail.com>
2013-10-09 08:15:26 -0400

    Cleardb

--- connect.php
+++ connect.php
@@ -8,11 +8,28 @@
 	$dbname = "sam926";
 */
 
+/*
 	$hostname = "localhost";
 	$username = "root";
 	$password = "";
 	$dbname = "sam926";
+*/
 
+// START Heroku ClearDB
+
+    $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"],1);
+
+    mysql_connect($server, $username, $password);
+            
+    
+    mysql_select_db($db);
+
+// END Heroku ClearDB
 
 //Connecting to your database
             mysql_connect($hostname, $username, $password) OR DIE ("Unable to
