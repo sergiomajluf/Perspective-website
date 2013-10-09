ffc549fe69ff35b2a3f11d88e3b1bd2769402366
Sergio Majluf <sergio.majluf@gmail.com>
2013-10-09 15:03:34 -0400

    db fix

--- connect.php
+++ connect.php
@@ -1,5 +1,6 @@
 <?php
 
+print_r($_POST);
 
     $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 
@@ -9,16 +10,12 @@
     $db = substr($url["path"],1);
 
 
-    mysql_connect($server, $username, $password);
+    mysql_connect($server, $username, $password) OR DIE ("Unable to
+            connect to database! Please try again later.");
+
             
     
-    mysql_select_db($db);
+    mysql_select_db($db) ;
 
-// END Heroku ClearDB
-
-//Connecting to your database
-            mysql_connect($hostname, $username, $password) OR DIE ("Unable to
-            connect to database! Please try again later.");
-            mysql_select_db($dbname);
 
 ?>
